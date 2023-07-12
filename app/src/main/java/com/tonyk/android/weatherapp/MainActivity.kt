290301cd9e4dad62fb62d3f563e160d6480f61fe
AntonKo24 <anton.korieniev.dev@gmail.com>
2023-07-12 22:47:11 +0300

    First commit with basic UI that still needs some work.

---
+++ app/src/main/java/com/tonyk/android/weatherapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tonyk.android.weatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
